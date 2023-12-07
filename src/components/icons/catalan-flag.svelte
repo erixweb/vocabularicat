b1dffe2da16ee2a2c88edc83013587a0147c7fdf
erixweb <erikmorenodev@gmail.com>
2023-12-07 12:48:50 +0100

    Add language selection

---
+++ src/components/icons/catalan-flag.svelte
@@ -0,0 +1,30 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	id="svg583"
+	viewBox="0 0 1260 810"
+	version="1.0"
+	y="0"
+	x="0"
+	width="40px"
+	height="40px"
+>
+	<rect id="rect586" style="fill-rule:evenodd;fill:#DA121A" height="90" width="1260" y="90" x="0"
+	></rect>
+	<rect id="rect587" style="fill-rule:evenodd;fill:#DA121A" height="90" width="1260" y="270" x="0"
+	></rect>
+	<rect id="rect588" style="fill-rule:evenodd;fill:#DA121A" height="90" width="1260" y="450" x="0"
+	></rect>
+	<rect id="rect589" style="fill-rule:evenodd;fill:#DA121A" height="90" width="1260" y="630" x="0"
+	></rect>
+	<rect id="rect590" style="fill-rule:evenodd;fill:#FCDD09" height="90" width="1260" y="720" x="0"
+	></rect>
+	<rect id="rect591" style="fill-rule:evenodd;fill:#FCDD09" height="90" width="1260" y="360" x="0"
+	></rect>
+	<rect id="rect592" style="fill-rule:evenodd;fill:#FCDD09" height="90" width="1260" y="540" x="0"
+	></rect>
+	<rect id="rect593" style="fill-rule:evenodd;fill:#FCDD09" height="90" width="1260" y="0" x="0"
+	></rect>
+	<rect id="rect594" style="fill-rule:evenodd;fill:#FCDD09" height="90" width="1260" y="180" x="0"
+	></rect>
+</svg>
---
+++ src/components/language-button.svelte
@@ -0,0 +1,26 @@
+<script>
+	import CatalanFlag from "./icons/catalan-flag.svelte"
+    import EnglishFlag from "./icons/english-flag.svelte"
+    import SpanishFlag from "./icons/spanish-flag.svelte"
+
+    let show = false
+</script>
+
+<div class="relative">
+    <button on:click={() => show = !show} class="flex gap-[10px] items-center justify-center">
+        <CatalanFlag />
+        Català
+    </button>
+    {#if show}
+    <div class="absolute top-[40px] rounded-[5px] bg-slate-200 p-[8px] px-[16px] flex gap-[20px] flex-col">
+        <a href="/es" class="flex gap-[10px] items-center justify-center">
+            <SpanishFlag />
+            Español
+        </a>
+        <a href="/en" class="flex gap-[10px] items-center justify-center">
+            <EnglishFlag />
+            English
+        </a>
+    </div>
+    {/if}
+</div>
